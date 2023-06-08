c4cb6674c5b537385ca0ad8d02ba4851eb77ffd8
Nithin <nithindevathi@gmail.com>
2023-06-08 12:48:23 +0530

    Added React router

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
 
 makeServer();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>
 );
 
