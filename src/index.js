0e8f55e5900214f136d31fa2f3b9b6477cf7bbe1
Nithin <nithindevathi@gmail.com>
2023-06-09 11:31:50 +0530

    Added new contexts

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AuthProvider } from './Services/HelperFunc/AuthFunc';
+import { AuthProvider } from "./Services/HelperFunc/AuthFunc";
+import { PostsProvider } from "./Services/HelperFunc/PostFunc";
+import { UserProvider } from "./Services/HelperFunc/UsersFunc";
 makeServer();
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Router>
     <AuthProvider>
-    <Router>
-    <App />
-    </Router>
+      <UserProvider>
+        <PostsProvider>
+          <App />
+        </PostsProvider>
+      </UserProvider>
     </AuthProvider>
-  </React.StrictMode>
+  </Router>
 );
 
 reportWebVitals();
