bb750264b02f748bd9e59852a1c9c2ca0e270d8b
Nithin <nithindevathi@gmail.com>
2023-06-09 12:27:17 +0530

    Create MainReducer.js

---
+++ src/Services/Context/MainReducer.js
@@ -0,0 +1,68 @@
+import { createContext,useEffect,useReducer } from "react"
+import axios from "axios";
+
+export const MainContext=createContext()
+export function ReducerProv({children})
+{
+    useEffect(()=>
+    {
+        const intialData=async()=>
+        {
+            const response = await axios.get(`/api/posts`)
+            dispatcherMain({type:"getPosts",payload:response.data.posts})
+            const resUsers = await axios.get("/api/users")
+            dispatcherMain({type:"getUsers",payload:resUsers.data.users})
+        }
+        intialData()
+    },[])
+    const MainData={
+        ExistedUsers:[],
+        isLoggedin:false,
+        loggedInUser:{fname:"",lname:"",username:"",bio:"",portFolio:"",avatar:""},
+        bookmarks:[],
+        posts:[],
+    }
+    const [state, dispatcherMain] = useReducer(MainFun, MainData);
+
+    function MainFun(state,action)
+    {
+        if(action.type==="getPosts")
+        {
+            return {...state,posts:action.payload}
+        }
+        else if(action.type==="getUsers")
+        {
+            return {...state,ExistedUsers:action.payload}
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return(<>
+        <MainContext.Provider value={{dispatcherMain,Posts:state.posts,Users:state.ExistedUsers}}>{children}</MainContext.Provider>
+    </>)
+}
