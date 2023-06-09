ecbb0fc41b1e2c4b4e655576ee2a5a3eb7f478e3
Nithin <nithindevathi@gmail.com>
2023-06-09 12:27:45 +0530

    showing posts in homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,11 @@
 
 import { useContext } from "react"
-import { PostsContext } from "../../Services/HelperFunc/PostFunc"
+import { MainContext } from "../../Services/Context/MainReducer"
+import "./home.css"
 
 export function Home1()
 {
-    const {getPosts}=useContext(PostsContext)
-    getPosts()
+    const {Posts,Users}=useContext(MainContext)
     return(<div>
         <section className="home">
             <div className="home_pages">
@@ -18,8 +18,17 @@ export function Home1()
                     <p>userName</p>
                 </div>
             </div>
-            <div className="home_posts"></div>
-            <div className="home_followers"></div>
+            <div className="home_posts">
+                {Posts.map((val)=><ul key={val._id}>
+                    <li>{val.username}</li>
+                    <li>{val.content}</li>
+                    <li>{val.createdAt}</li>
+                    <li>{val.likes.likeCount}</li>
+                </ul>)}
+            </div>
+            <div className="home_followers">
+                {Users.map}
+            </div>
         </section>
     </div>)
 }
