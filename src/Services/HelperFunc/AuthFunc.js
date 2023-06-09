a1405fa7c830132f19b9fc61fecd50dc77b94279
Nithin <nithindevathi@gmail.com>
2023-06-09 10:21:50 +0530

    Added login and signup function

---
+++ src/Services/HelperFunc/AuthFunc.js
@@ -0,0 +1,66 @@
+import { createContext } from "react";
+
+import axios from "axios";
+import {  ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+export const AuthContext = createContext();
+
+
+export function AuthProvider({ children }) {
+
+  function LoginHandler({userName,pwd}) {
+    console.log(userName,pwd)
+    const loginHandle1 = async () => {
+      // console.log(userName,pwd)
+        try {
+            const response = await axios.post(`/api/auth/login`, {
+                username:userName,
+                password: pwd
+              });
+              console.log(response.data)
+              if(response.status===201)
+              {
+                  toast.success(`Welcome Back`,{
+                  position:"top-center"})
+                        // nav("/Login1")
+              }
+        } catch (error) {
+            console.log(error)
+        }     
+    
+  }
+  loginHandle1();
+}
+
+function Signup(userDetails)
+      {
+        
+                  const signupHandler = async () => {
+                    try {
+                      const response = await axios.post(`/api/auth/signup`, {
+                        firstName: userDetails?.fName,
+                        lastName: userDetails?.lName,
+                        username: userDetails?.userName,
+                        password: userDetails?.pwd,
+                      });
+                      console.log(response)
+                      localStorage.setItem("token", response.data.encodedToken);
+                      if(response.status===201)
+                      {
+                        toast.success(`Welcome ${userDetails.fName}`,{
+                          position:"top-center"})
+                        // nav("/Login1")
+                      }
+                      
+                    } catch (error) {
+                      console.log(error);
+                    }
+                   
+                    
+                  };
+                 
+                  signupHandler()
+      }    
+  return (<><AuthContext.Provider value={{ LoginHandler,Signup }}>{children}</AuthContext.Provider>
+  <ToastContainer></ToastContainer></>);
+}
