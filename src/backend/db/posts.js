230d98d958e64aeecfb75ca77bf1369804d475d0
Nithin <nithindevathi@gmail.com>
2023-06-09 11:05:54 +0530

    Added new posts

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -10,20 +10,21 @@ export const posts = [
   {
     _id: uuid(),
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+      "Demon slayer episode Kyojuro Rengoku vs Akaza was stunning and emotional fight",
     likes: {
       likeCount: 0,
       likedBy: [],
       dislikedBy: [],
     },
-    username: "adarshbalika",
+    username: "tony@gmail.com",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    comment:[]
   },
   {
     _id: uuid(),
     content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+      "Rewatching battle between naruto vs pain",
     likes: {
       likeCount: 0,
       likedBy: [],
@@ -32,5 +33,49 @@ export const posts = [
     username: "shubhamsoni",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    comment:[]
+  },
+  {
+    _id: uuid(),
+    content:
+      "waiting for Attack on Titan Final episode",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+      
+    },
+    username: "shubhamsoni",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comment:[]
+  },
+  {
+    _id: uuid(),
+    content:
+      "Hope soon vegeta will get ultra instinct",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "shubhamsoni",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comment:[]
+  },
+  {
+    _id: uuid(),
+    content:
+      "Waiting for Satoru Gojo to unleash his full power",
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    username: "shubhamsoni",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    comment:[]
   },
 ];
