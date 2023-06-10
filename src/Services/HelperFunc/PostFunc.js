8ed7b6aaac8f7dbc92cf098ae5bce4813386b196
Nithin <nithindevathi@gmail.com>
2023-06-10 17:59:26 +0530

    Added like functionality for posts

--- src/Services/HelperFunc/PostFunc.js
+++ src/Services/HelperFunc/PostFunc.js
@@ -1,10 +1,12 @@
-import { createContext } from "react"
+import { createContext,useContext } from "react"
 import axios from "axios";
-
+import { MainContext } from "../Context/MainReducer";
 
 export const PostsContext=createContext()
 export function PostsProvider({children})
 {
+    const encodedToken=localStorage.getItem("token")
+    const {dispatcherMain}=useContext(MainContext)
     const getPosts=()=>
     {
         const posts=async()=>
@@ -15,6 +17,7 @@ export function PostsProvider({children})
                   if(response.status===200)
                   {
                     console.log(response.data.posts)
+                    dispatcherMain({type:"getPosts",payload:response.data.posts})
                   }
             } catch (error) {
                 console.log(error)
@@ -22,8 +25,26 @@ export function PostsProvider({children})
         }
         posts()
     }
+    function likePosts(postId)
+    {
+        const like=async()=>
+        {
+            try {
+                const response=await axios.post(`/api/posts/like/${postId}`,{},{
+                    headers: {
+                        authorization: encodedToken, 
+                      },
+                })
+                console.log(response.data.posts)
+                dispatcherMain({type:"getPosts",payload:response.data.posts})
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        like()
+    }
 
     return(<>
-    <PostsContext.Provider value={{getPosts}}>{children}</PostsContext.Provider>
+    <PostsContext.Provider value={{getPosts,likePosts}}>{children}</PostsContext.Provider>
     </>)
 }
