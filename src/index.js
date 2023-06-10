260cf6824c3dc896251c2f1f41fb460e22aad1a4
Nithin <nithindevathi@gmail.com>
2023-06-10 12:25:59 +0530

    Added new Contexts

--- src/index.js
+++ src/index.js
@@ -8,17 +8,23 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./Services/HelperFunc/AuthFunc";
 import { PostsProvider } from "./Services/HelperFunc/PostFunc";
 import { UserProvider } from "./Services/HelperFunc/UsersFunc";
+import { ReducerProv } from "./Services/Context/MainReducer";
+import { FollowProvider } from "./Services/HelperFunc/FollowFunc";
 makeServer();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <AuthProvider>
-      <UserProvider>
-        <PostsProvider>
-          <App />
-        </PostsProvider>
-      </UserProvider>
-    </AuthProvider>
+    <ReducerProv>
+      <AuthProvider>
+        <FollowProvider>
+        <UserProvider>
+          <PostsProvider>
+            <App />
+          </PostsProvider>
+        </UserProvider>
+        </FollowProvider>
+      </AuthProvider>
+    </ReducerProv>
   </Router>
 );
 
