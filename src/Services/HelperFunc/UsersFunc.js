5ee0be8e805964feae830db7d10478286b3b819b
Nithin <nithindevathi@gmail.com>
2023-06-16 10:13:47 +0530

    added edit user function

--- src/Services/HelperFunc/UsersFunc.js
+++ src/Services/HelperFunc/UsersFunc.js
@@ -5,7 +5,8 @@ import { MainContext } from "../Context/MainReducer";
 export const UsersContext=createContext()
 export function UserProvider({children})
 {
-    const {dispatcherMain,loggedInUserFollwers}=useContext(MainContext)
+    const {dispatcherMain,loggedInUserFollwers,Users}=useContext(MainContext)
+    const encodedToken=localStorage.getItem("token")
     // function getUsers()
     // {
     //     const users=async()=>
@@ -24,7 +25,22 @@ export function UserProvider({children})
         const check=loggedInUserFollwers.find((user)=>user===userName)
         return check===undefined?false:true
     }
+
+        const editUsers=async(newDetails)=>
+        {
+            try {
+                const response=await axios.post(`/api/users/edit`,{ userData:newDetails},{
+                    headers: {
+                        authorization: encodedToken, 
+                      },
+                })
+                dispatcherMain({type:"userDetails",payload:response.data.user})
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    
     return(<>
-    <UsersContext.Provider value={{followersExist}} >{children}</UsersContext.Provider>
+    <UsersContext.Provider value={{followersExist,editUsers}} >{children}</UsersContext.Provider>
     </>)
 }
