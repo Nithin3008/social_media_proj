b116411f4ce501ed0c13f40f84a53cbc7b8acff6
Nithin <nithindevathi@gmail.com>
2023-06-20 16:39:11 +0530

    Added profile section
    Reformatted code

--- src/components/suggestion/SuggestionSec.js
+++ src/components/suggestion/SuggestionSec.js
@@ -1,24 +1,51 @@
-import "./Suggestionsec.css"
-import { MainContext } from "../../Services/Context/MainReducer"
-import { FollowContext } from "../../Services/HelperFunc/FollowFunc"
-import { UsersContext } from "../../Services/HelperFunc/UsersFunc"
-import { useContext } from "react"
-export function Suggested()
-{
-    const {Users}=useContext(MainContext)
-    const {setFollowers,setUnFollowers}=useContext(FollowContext)
-    const {followersExist}=useContext(UsersContext)
-    return(<div>
-        <section>
+import "./Suggestionsec.css";
+import { MainContext } from "../../Services/Context/MainReducer";
+import { FollowContext } from "../../Services/HelperFunc/FollowFunc";
+import { UsersContext } from "../../Services/HelperFunc/UsersFunc";
+import { useContext } from "react";
+import { useNavigate } from "react-router";
+export function Suggested() {
+  const { Users, loggedInUser } = useContext(MainContext);
+  const { setFollowers, setUnFollowers } = useContext(FollowContext);
+  const { followersExist } = useContext(UsersContext);
+  const nav = useNavigate();
+  return (
+    <div>
+      <section>
+        <div
+          className="UserProfile"
+          onClick={() => nav(`/Profile1/${loggedInUser.username}`)}
+        >
+          <div>
+            <img src={loggedInUser.avatar}></img>
+          </div>
+          <div>
+            <p>{loggedInUser.firstName}</p>
+            <p>{loggedInUser.username}</p>
+          </div>
+        </div>
         <div className="Suggest_followers">
-                <h1>Suggested Users</h1>
-            {Users.map((val)=><ul key={val._id}>
-                    <p><li>{val.firstName}</li>
-                    <span   >{val.username}</span></p>
-                    
-                    <button onClick={()=>followersExist(val.username)?setUnFollowers(val._id):setFollowers(val._id)}>{followersExist(val.username)?"unfollow":"Follow"}</button>
-                </ul>)}
-            </div>
-        </section>
-               </div>)
+          <h1>Suggested Users</h1>
+          {Users.map((val) => (
+            <ul key={val._id}>
+              <p>
+                <li>{val.firstName}</li>
+                <span>{val.username}</span>
+              </p>
+
+              <button
+                onClick={() =>
+                  followersExist(val.username)
+                    ? setUnFollowers(val._id)
+                    : setFollowers(val._id)
+                }
+              >
+                {followersExist(val.username) ? "unfollow" : "Follow"}
+              </button>
+            </ul>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
