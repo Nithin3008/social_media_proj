df7ce796615b3dbcb68aed232f6560f8b36f3a25
Nithin <nithindevathi@gmail.com>
2023-06-23 12:04:47 +0530

    reformatted code, removed unwanted svg

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,7 +6,6 @@ import { Suggested } from "../../components/suggestion/SuggestionSec";
 import { RouteSec } from "../../components/RouteSection/RouteSec";
 import { useNavigate } from "react-router";
 import { UsersContext } from "../../Services/HelperFunc/UsersFunc";
-// import Modal from "react-overlays/Modal";
 
 export function Home1() {
   const { Posts, loggedInUser, loggedInUserFollwers } = useContext(MainContext);
@@ -21,13 +20,13 @@ export function Home1() {
     editPost,
     deletePost,
     sortByDate,
-    Trending
+    Trending,
   } = useContext(PostsContext);
   const [show, setShow] = useState(false);
   const [editObject, setObject] = useState({});
   const [editDetails, setDetails] = useState();
   const [editImage, setImage] = useState();
-  const [showEdit,setShowEdit]=useState("")
+  const [showEdit, setShowEdit] = useState("");
   const { userAvatars } = useContext(UsersContext);
   const nav = useNavigate();
   const postsD = [...Posts].filter(
@@ -98,31 +97,54 @@ export function Home1() {
             <button onClick={() => checkValue()}>Post</button>
           </div>
           <div className="home_postsSection-filters">
-            <button onClick={()=>Trending()}>Trending🔥🔥</button>
+            <button onClick={() => Trending()}>Trending🔥🔥</button>
             <button onClick={() => sortByDate()}>Sort By Date</button>
           </div>
 
           {PostsData.map((val) => (
             <ul key={val._id} className="home__postsSection__posts">
               <li className="home__postsSection__posts-fName">
-                <img  onClick={() => nav(`/Profile1/${val.username}`)} src={userAvatars(val.username)}></img>
+                <img
+                  onClick={() => nav(`/Profile1/${val.username}`)}
+                  src={userAvatars(val.username)}
+                ></img>
                 {val.firstName} <span>{val.username}</span>
-                <p className="dotsForFunc" style={{ marginLeft: "150px" }}> 
-                <svg onClick={()=>showEdit===val._id?setShowEdit(""):setShowEdit(val._id)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#edf1ff" strokeWidth="2" strokeLinecap="square" strokeLinejoin="round"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
-                <li style={{display:val._id===showEdit?"block":"none"}}>
-                <button
-                  
-                  onClick={(e) => {
-                    setShow(!show);
-                    setObject(val);
-                    setDetails(val.content);
-                    setImage(val.img);
-                  }}
-                >
-                  Edit
-                </button>
-                <button onClick={() => deletePost(val._id)}>Delete</button>
-                </li>
+                <p className="dotsForFunc" style={{ marginLeft: "150px" }}>
+                  <svg
+                    onClick={() =>
+                      showEdit === val._id
+                        ? setShowEdit("")
+                        : setShowEdit(val._id)
+                    }
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#edf1ff"
+                    strokeWidth="2"
+                    strokeLinecap="square"
+                    strokeLinejoin="round"
+                  >
+                    <circle cx="12" cy="12" r="1"></circle>
+                    <circle cx="19" cy="12" r="1"></circle>
+                    <circle cx="5" cy="12" r="1"></circle>
+                  </svg>
+                  <li
+                    style={{ display: val._id === showEdit ? "block" : "none" }}
+                  >
+                    <button
+                      onClick={(e) => {
+                        setShow(!show);
+                        setObject(val);
+                        setDetails(val.content);
+                        setImage(val.img);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button onClick={() => deletePost(val._id)}>Delete</button>
+                  </li>
                 </p>
               </li>
               <li>
@@ -176,19 +198,6 @@ export function Home1() {
                   stroke-linejoin="round"
                 >
                   <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="#fa005a"
-                  stroke-width="2"
-                  stroke-linecap="square"
-                  stroke-linejoin="round"
-                >
-                  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
                 </svg>
               </li>
             </ul>
