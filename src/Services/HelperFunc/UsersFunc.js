68a51ebe81b1ca6fba9f0d81ce803b40d238ba63
Nithin <nithindevathi@gmail.com>
2023-06-24 15:26:00 +0530

    Added new avatars data

--- src/Services/HelperFunc/UsersFunc.js
+++ src/Services/HelperFunc/UsersFunc.js
@@ -12,34 +12,42 @@ export function UserProvider({ children }) {
     const check = loggedInUserFollwers.find((user) => user._id=== userId);
     return check === undefined ? false : true;
   }
+  const dpData=[
+    "https://github.com/Nithin3008/social_media_proj/blob/master/public/images/dp1.jpeg?raw=true",
+    "https://github.com/Nithin3008/social_media_proj/blob/master/public/images/dp2.jpeg?raw=true",
+    "https://github.com/Nithin3008/social_media_proj/blob/master/public/images/dp3.jpg?raw=true",
+    "https://github.com/Nithin3008/social_media_proj/blob/master/public/images/dp4.png?raw=true",
+    "https://github.com/Nithin3008/social_media_proj/blob/master/public/images/dp6.jpg?raw=true",
+    ,"https://github.com/Nithin3008/social_media_proj/blob/master/public/images/dp5.jpg?raw=true"
+  ]
   async function uploadImage(post) {
-    try {
-      const file = post.img;
-      console.log(file);
-      console.log(file, "image path");
-      const present_key = "social_media_proj";
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("upload_preset", present_key);
-      if (post.img) {
-        const res = await fetch(
-          `https://api.cloudinary.com/v1_1/king-cloud/image/upload`,
-          {
-            method: "POST",
-            body: formData,
+      
+        try {      
+          const file = post.img;
+          const present_key = "social_media_proj";
+          const formData = new FormData();
+          formData.append("file", file);
+          formData.append("upload_preset", present_key);
+          if (post.img) {
+            const res = await fetch(
+              `https://api.cloudinary.com/v1_1/king-cloud/image/upload`,
+              {
+                method: "POST",
+                body: formData,
+              }
+            );
+            const x = await res.json();
+            console.log(x.url);
+            post.avatar = x.url;
+          } else {
+            return { ...post, img: null };
           }
-        );
-        const x = await res.json();
-        console.log(x.url);
-        post.avatar = x.url;
-      } else {
-        return { ...post, img: null };
-      }
-    } catch (error) {
-      console.log(error);
-    }
-
-    return post;
+        } catch (error) {
+          console.log(error);
+        }
+    
+        return post;
+    
   }
   const editUser = async (newDetails) => {
     try {
@@ -59,9 +67,7 @@ export function UserProvider({ children }) {
     }
   };
   async function editUsers(User) {
-    console.log(User);
     const result1 = await uploadImage(User);
-    console.log(result1);
     const result2 = await editUser(result1);
   }
 
@@ -71,7 +77,7 @@ export function UserProvider({ children }) {
   }
   return (
     <>
-      <UsersContext.Provider value={{ followersExist, editUsers, userAvatars }}>
+      <UsersContext.Provider value={{ followersExist, editUsers, userAvatars,dpData }}>
         {children}
       </UsersContext.Provider>
     </>
